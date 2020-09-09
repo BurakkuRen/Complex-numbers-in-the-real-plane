ce96ba7ac8c8f5cf5a7000515ace8d3b29d164e8
BurakkuRen <71036982+BurakkuRen@users.noreply.github.com>
2020-09-09 22:44:50 +0200

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -3,5 +3,20 @@ import itertools
 grado = 0
 coeficiente = 0
 
-rangoCoef = [x for x in range(-coeficiente,coeficiente+1)]
-combinaciones = list(itertools.product(rangoCoef,repeat=grado+1)) 
+def products(grado,coeficiente):
+  rangoCoef = [x for x in range(-coeficiente,coeficiente+1)]
+  combinaciones = list(itertools.product(rangoCoef,repeat=grado+1)) 
+
+
+#el algoritmo que las soluciona:
+roots = []
+
+for comb in coeficients:
+    p = np.poly1d(comb)
+    for n in p.r:
+        roots.append(n)
+OR
+
+for comb in coeficients:
+    p = np.poly1d(comb)
+    roots += n
